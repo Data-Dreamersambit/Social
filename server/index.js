913915a6207b2a831881640accf544aea2c87b82
Sambit Sahoo <sambitsahoo191@gmail.com>
2025-11-09 06:35:41 +0530

    port

--- server/index.js
+++ server/index.js
@@ -63,6 +63,7 @@ app.get(/.*/, (_, res) => {
 const server = http.createServer(app);
 initSocket(server);
 
+const PORT = process.env.PORT || 5000;
  
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
